542a0f6edebddf72c76379090556032770114cc9
Madara228 <karagishiev14@mail.ru>
2018-08-06 13:35:36 +0300

    motion blur effect

---
+++ Assets/Scripts/collisionScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionScript : MonoBehaviour {
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
