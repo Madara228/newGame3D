5f0a69ef119c87e1297530e9b27d1b2cf1478981
Madara228 <karagishiev14@mail.ru>
2018-08-07 10:19:17 +0300

    adding shoot

---
+++ Assets/Scripts/bulletScipt.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScipt : MonoBehaviour {
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        //
+    }
+}
--- Assets/Scripts/newPlayerScript.cs
+++ Assets/Scripts/newPlayerScript.cs
@@ -8,6 +8,8 @@ public class newPlayerScript : MonoBehaviour {
     #region public vars
     public float speed = 10f;
     public float rotateSpeed = 90f;
+    public GameObject bullet;
+    public Transform firePos;
     #endregion
     #region private vars
     [SerializeField] Rigidbody rb;
@@ -27,6 +29,15 @@ public class newPlayerScript : MonoBehaviour {
         movement.z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         rotation = Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime;
         Debug.Log(isGround);
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            GameObject Temporary_Bullet;
+            Temporary_Bullet = Instantiate(bullet, firePos.position,firePos.transform.rotation) as GameObject;
+            Temporary_Bullet.transform.Rotate(Vector3.left * 90);
+            Rigidbody rigid = Temporary_Bullet.GetComponent<Rigidbody>();
+            rigid.AddForce(transform.forward *1000f);
+            Destroy(Temporary_Bullet, 5f);
+        }
     }
     private void OnCollisionStay(Collision collision)
     {
@@ -35,7 +46,7 @@ public class newPlayerScript : MonoBehaviour {
     private void FixedUpdate()
     {
         transform.Translate(movement, Space.Self);
-        transform.Rotate(0f,rotation,0f);
+        transform.Rotate(transform.rotation.x,rotation,transform.rotation.z);
 
         if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
         {
@@ -43,7 +54,5 @@ public class newPlayerScript : MonoBehaviour {
             rb.velocity = Vector3.up * jumpspeed * Time.deltaTime;
             isGround = false;
         }
-        
-
     }
 }
